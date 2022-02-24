214e06628c40e8e165fd7fef8dc03df10ec88457
Svetlana Ovsiannikova <springtime070@mail.ru>
2022-02-24 16:41:39 +0300

    Добавляет правки в первые функции

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 //Функция, возвращающая случайное целое число из переданного диапазона включительно
 //Использованы данные с сайта https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Statements/throw
-
 function getRandomInt(min, max) {
-  if (min < 0) {throw 'Incorrect \'min\' value';}
+  if (min < 0) {
+    throw 'Incorrect \'min\' value';
+  }
+
   min = Math.ceil(min);
   max = Math.floor(max);
+
   return min + Math.floor(Math.random() * (max-min+1));
 }
 
@@ -13,7 +16,6 @@ getRandomInt(1, 7);
 
 //Функция для проверки максимальной длины строки
 //Использованы данные с сайта https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/length
-
 function checkMaxLength(comment, maxLength) {
   return comment.length <= maxLength;
 }
